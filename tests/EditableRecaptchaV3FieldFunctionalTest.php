08b8928a54f6b4360bf3dbc2c915a10b56b8eb34
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-01-31 16:57:14 +1100

    Update tests to take into account rule handling

--- tests/EditableRecaptchaV3FieldFunctionalTest.php
+++ tests/EditableRecaptchaV3FieldFunctionalTest.php
@@ -2,8 +2,10 @@
 
 namespace NSWDPC\SpamProtection\Tests;
 
+use NSWDPC\SpamProtection\Logger;
 use NSWDPC\SpamProtection\Verifier;
 use NSWDPC\SpamProtection\RecaptchaV3Field;
+use NSWDPC\SpamProtection\RecaptchaV3Rule;
 use NSWDPC\SpamProtection\EditableRecaptchaV3Field;
 use NSWDPC\SpamProtection\SubmittedRecaptchaV3Field;
 use SilverStripe\Core\Injector\Injector;
@@ -11,6 +13,7 @@ use SilverStripe\UserForms\Control\UserDefinedFormController;
 use SilverStripe\Dev\FunctionalTest;
 use SilverStripe\UserForms\Model\Recipient\EmailRecipient;
 use SilverStripe\UserForms\Model\UserDefinedForm;
+use SilverStripe\Security\SecurityToken;
 
 /**
  * Functional tests for {@link NSWDPC\SpamProtection\EditableRecaptchaV3Field}
@@ -62,20 +65,11 @@ class EditableRecaptchav3FieldFunctionalTest extends FunctionalTest
     public function testProcessIncludeInEmails()
     {
 
-        // and test verifier
+        // Use the test verifier
         $verifier = TestVerifier::create();
         $verifier->setIsHuman(true);
         Injector::inst()->registerService(
             $verifier, Verifier::class
-        );
-
-        $field = TestRecaptchaV3Field::create('include_in_emails');
-        $field->setVerifier($verifier);
-
-        // use the test field
-        Injector::inst()->registerService(
-            $field,
-            RecaptchaV3Field::class
         );
 
         $userDefinedForm = $this->setupFormFrontend('include-in-emails');
@@ -96,19 +90,25 @@ class EditableRecaptchav3FieldFunctionalTest extends FunctionalTest
         // load the form
         $page = $this->get( $userDefinedForm->Link() );
 
-        // check the form exists
+        // check the field exists
         $form = $controller->Form();
-
         $captchaField = $form->HiddenFields()->fieldByName('include_in_emails');
 
-        $this->assertInstanceOf( TestRecaptchaV3Field::class, $captchaField, "include_in_emails is a TestRecaptchaV3Field" );
+        $this->assertInstanceOf( RecaptchaV3Field::class, $captchaField, "include_in_emails is a RecaptchaV3Field" );
 
-        $data = [];
-        $response = $this->submitForm('UserForm_Form_' . $userDefinedForm->ID, null, $data);
+        $this->assertInstanceOf( TestVerifier::class, $captchaField->getVerifier(), "Verifier is the TestVerifier" );
+
+        $token = 'token_include_in_emails';
+        $data = [
+            $captchaField->getName() => $token,
+            'SecurityID' => SecurityToken::getSecurityID()
+        ];
+        // Have to post with token value as submitForm does not allow HiddenField values to be set
+        $response = $this->post($form->FormAction(), $data);
 
         $submittedFields = SubmittedRecaptchaV3Field::get()->filter(['Name' => 'include_in_emails']);
 
-        $this->assertTrue($submittedFields->count() == 1, "One SubmittedRecaptchaV3Field for include_in_emails");
+        $this->assertEquals(1, $submittedFields->count(), "One SubmittedRecaptchaV3Field for include_in_emails");
 
         $submittedField = $submittedFields->first();
 
@@ -148,15 +148,6 @@ class EditableRecaptchav3FieldFunctionalTest extends FunctionalTest
             $verifier, Verifier::class
         );
 
-        $field = TestRecaptchaV3Field::create('not_include_in_emails');
-        $field->setVerifier($verifier);
-
-        // use the test field
-        Injector::inst()->registerService(
-            $field,
-            RecaptchaV3Field::class
-        );
-
         $userDefinedForm = $this->setupFormFrontend('not-include-in-emails');
 
         $recipients = $userDefinedForm->EmailRecipients();
@@ -180,14 +171,18 @@ class EditableRecaptchav3FieldFunctionalTest extends FunctionalTest
 
         $captchaField = $form->HiddenFields()->fieldByName('not_include_in_emails');
 
-        $this->assertInstanceOf( TestRecaptchaV3Field::class, $captchaField, "not_include_in_emails is a TestRecaptchaV3Field" );
+        $this->assertInstanceOf( RecaptchaV3Field::class, $captchaField, "not_include_in_emails is a RecaptchaV3Field" );
 
-        $data = [];
-        $response = $this->submitForm('UserForm_Form_' . $userDefinedForm->ID, null, $data);
+        $token = 'token_not_include_in_emails';
+        $data = [
+            $captchaField->getName() => $token,
+            'SecurityID' => SecurityToken::getSecurityID()
+        ];
+        $response = $this->post($form->FormAction(), $data);
 
         $submittedFields = SubmittedRecaptchaV3Field::get()->filter(['Name' => 'not_include_in_emails']);
 
-        $this->assertTrue($submittedFields->count() == 1, "One SubmittedRecaptchaV3Field for not_include_in_emails");
+        $this->assertEquals(1, $submittedFields->count(), "One SubmittedRecaptchaV3Field for not_include_in_emails");
 
         $submittedField = $submittedFields->first();
 
@@ -213,5 +208,186 @@ class EditableRecaptchav3FieldFunctionalTest extends FunctionalTest
         $this->assertFalse(strpos($email['Content'], $title) !== false, 'Email contains the field name');
         $this->assertFalse(strpos($email['Content'], $value) !== false, 'Email contains the field value');
     }
+
+
+    /**
+     * Functional test for IncludeInEmails=1 value
+     */
+    public function testProcessWithRuleAttachedToEditableField()
+    {
+
+        // create a rule
+        $rule = RecaptchaV3Rule::create([
+            'Tag' => 'editable-field-rule',
+            'Score' => 55,
+            'Action' => 'editablefieldrule/submit',
+            'ActionToTake' => 'Block',
+            'Enabled' => 1,
+            'AutoCreated' =>  0
+        ]);
+        $rule->write();
+
+        // Use the test verifier
+        $verifier = TestVerifier::create();
+        $verifier->setIsHuman(true);
+        Injector::inst()->registerService(
+            $verifier, Verifier::class
+        );
+
+        $userDefinedForm = $this->setupFormFrontend('test-field-with-rule');
+
+        $recipients = $userDefinedForm->EmailRecipients();
+        $this->assertEquals(1, $recipients->count(), "UserDefinedForm has one EmailRecipient");
+
+        $recipient = $recipients->first();
+        $this->assertEquals('test.include.rule@example.com', $recipient->EmailAddress, "EmailRecipient has correct address");
+
+        $this->assertInstanceOf(UserDefinedForm::class,  $userDefinedForm, "Form is a UserDefinedForm");
+
+        $editableRecaptchaV3Field = $userDefinedForm->Fields()->filter(['Name' => 'field_with_rule'])->first();
+        $this->assertInstanceOf(EditableRecaptchaV3Field::class,  $editableRecaptchaV3Field, "Field is a EditableRecaptchaV3Field");
+
+        $editableRecaptchaV3Field->RuleID = $rule->ID;
+        $editableRecaptchaV3Field->write();
+        $this->actWithPermission('ADMIN', function () use ($editableRecaptchaV3Field) {
+            $editableRecaptchaV3Field->doPublish();
+        });
+
+        $checkRule = $editableRecaptchaV3Field->Rule();
+        $this->assertEquals($rule->ID, $checkRule->ID, "Rules match");
+
+        $controller = new UserDefinedFormController($userDefinedForm);
+
+        $this->autoFollowRedirection = true;
+        $this->clearEmails();
+
+        // load the form
+        $page = $this->get( $userDefinedForm->Link() );
+
+        // check the field exists
+        $form = $controller->Form();
+        $captchaField = $form->HiddenFields()->fieldByName('field_with_rule');
+
+        $this->assertInstanceOf( RecaptchaV3Field::class, $captchaField, "field_with_rule is a RecaptchaV3Field" );
+
+        $this->assertInstanceOf( TestVerifier::class, $captchaField->getVerifier(), "Verifier is the TestVerifier" );
+
+        $token = 'token_check_rule';
+        $data = [
+            $captchaField->getName() => $token,
+            'SecurityID' => SecurityToken::getSecurityID()
+        ];
+
+        // Have to post with token value as submitForm does not allow HiddenField values to be set
+        $response = $this->post($form->FormAction(), $data);
+
+        $submittedFields = SubmittedRecaptchaV3Field::get()->filter(['Name' => 'field_with_rule']);
+
+        $this->assertEquals(1, $submittedFields->count(), "One SubmittedRecaptchaV3Field for include_in_emails");
+
+        $submittedField = $submittedFields->first();
+
+        $value = $submittedField->Value;
+        $title = $submittedField->Title;
+
+        $this->assertNotEmpty( $value, "Submitted verification field value empty" );
+        $this->assertNotEmpty( $title, "Submitted verification field title empty" );
+
+        $decodedValue = json_decode($value, true);
+
+        $this->assertNotEmpty($decodedValue);
+        $this->assertEquals( TestVerifier::RESPONSE_HUMAN_SCORE, $decodedValue['score'], "Score is " . TestVerifier::RESPONSE_HUMAN_SCORE);
+        $this->assertEquals( round($rule->Score/100, 2), $decodedValue['threshold'], "Threshold used in verification is the Rule score");
+        $this->assertEquals( 'localhost',  $decodedValue['hostname'], "Hostname is localhost");
+        $this->assertEquals( $rule->Action,  $decodedValue['action'], "Action is the Rule Action");
+
+        $email = $this->findEmail( $recipient->EmailAddress, $recipient->EmailReplyTo, $recipient->EmailSubject );
+
+        // check emails
+        $this->assertEmailSent( $recipient->EmailAddress, $recipient->EmailReplyTo, $recipient->EmailSubject );
+
+        $this->assertTrue(strpos($email['Content'], $recipient->EmailBodyHtml) !== false, 'Email contains the expected HTML string');
+        $this->assertTrue(strpos($email['Content'], $title) !== false, 'Email contains the field name');
+        $this->assertTrue(strpos($email['Content'], $value) !== false, 'Email contains the field value');
+
+    }
+
+
+    /**
+     * Functional test for IncludeInEmails=1 value
+     */
+    public function testProcessFailVerificationWithRuleAttachedToEditableField()
+    {
+
+        // create a rule
+        $rule = RecaptchaV3Rule::create([
+            'Tag' => 'editable-field-rule',
+            'Score' => 90,
+            'Action' => 'editablefieldruletofail/submit',
+            'ActionToTake' => 'Block',
+            'Enabled' => 1,
+            'AutoCreated' =>  0
+        ]);
+        $rule->write();
+
+        // Use the test verifier
+        $verifier = TestVerifier::create();
+        $verifier->setIsHuman(false);
+        Injector::inst()->registerService(
+            $verifier, Verifier::class
+        );
+
+        $userDefinedForm = $this->setupFormFrontend('test-field-with-rule');
+
+        $recipients = $userDefinedForm->EmailRecipients();
+        $this->assertEquals(1, $recipients->count(), "UserDefinedForm has one EmailRecipient");
+
+        $recipient = $recipients->first();
+        $this->assertEquals('test.include.rule@example.com', $recipient->EmailAddress, "EmailRecipient has correct address");
+
+        $this->assertInstanceOf(UserDefinedForm::class,  $userDefinedForm, "Form is a UserDefinedForm");
+
+        $editableRecaptchaV3Field = $userDefinedForm->Fields()->filter(['Name' => 'field_with_rule'])->first();
+        $this->assertInstanceOf(EditableRecaptchaV3Field::class,  $editableRecaptchaV3Field, "Field is a EditableRecaptchaV3Field");
+
+        $editableRecaptchaV3Field->RuleID = $rule->ID;
+        $editableRecaptchaV3Field->write();
+        $this->actWithPermission('ADMIN', function () use ($editableRecaptchaV3Field) {
+            $editableRecaptchaV3Field->doPublish();
+        });
+
+        $checkRule = $editableRecaptchaV3Field->Rule();
+        $this->assertEquals($rule->ID, $checkRule->ID, "Rules match");
+
+        $controller = new UserDefinedFormController($userDefinedForm);
+
+        $this->autoFollowRedirection = true;
+        $this->clearEmails();
+
+        // load the form
+        $page = $this->get( $userDefinedForm->Link() );
+
+        // check the field exists
+        $form = $controller->Form();
+        $captchaField = $form->HiddenFields()->fieldByName('field_with_rule');
+
+        $this->assertInstanceOf( RecaptchaV3Field::class, $captchaField, "field_with_rule is a RecaptchaV3Field" );
+
+        $this->assertInstanceOf( TestVerifier::class, $captchaField->getVerifier(), "Verifier is the TestVerifier" );
+
+        $token = 'token_check_rule';
+        $data = [
+            $captchaField->getName() => $token,
+            'SecurityID' => SecurityToken::getSecurityID()
+        ];
+
+        // Have to post with token value as submitForm does not allow HiddenField values to be set
+        $response = $this->post($form->FormAction(), $data);
+
+        $this->assertTrue( strpos($response->getBody(), RecaptchaV3Field::getMessagePossibleSpam()) !== false, "Message contains possible spam response" );
+
+
+    }
+
 
 }
--- tests/EditableRecaptchaV3FieldTest.php
+++ tests/EditableRecaptchaV3FieldTest.php
@@ -2,6 +2,8 @@
 
 namespace NSWDPC\SpamProtection\Tests;
 
+use NSWDPC\SpamProtection\RecaptchaV3Rule;
+use NSWDPC\SpamProtection\RecaptchaV3Field;
 use NSWDPC\SpamProtection\EditableRecaptchaV3Field;
 use NSWDPC\SpamProtection\SubmittedRecaptchaV3Field;
 use SilverStripe\Dev\SapphireTest;
@@ -16,7 +18,81 @@ class EditableRecaptchav3FieldTest extends SapphireTest
     /**
      * @var bool
      */
-    protected $usesDatabase = false;
+    protected $usesDatabase = true;
+
+    /**
+     * Test field return from getFormField
+     */
+    public function testGetFormField() {
+
+        $fieldScore = 32;
+        $fieldAction = "testgetformfield/submit";
+
+        $field = EditableRecaptchaV3Field::create();
+        $field->Title = "Test spam protection";
+        $field->IncludeInEmails = 0;
+        $field->Score = $fieldScore;
+        $field->Action = $fieldAction;
+        $field->write();
+
+        $formField = $field->getFormField();
+
+        $this->assertInstanceOf(RecaptchaV3Field::class, $formField, "Field is a RecaptchaV3Field");
+
+        $score = $formField->getScore();
+        $this->assertEquals(round($fieldScore / 100, 2), $score, "Score matches");
+
+        $action = $formField->getRecaptchaAction();
+        $this->assertEquals($fieldAction, $action, "Action matches");
+
+        $rule = $formField->getRecaptchaV3Rule();
+        $this->assertEmpty($rule, "No rule for field");
+    }
+
+    /**
+     * Test field return from getFormField when the field has a rule
+     */
+    public function testGetFormFieldWithRule() {
+
+        $fieldScore = 32;
+        $fieldAction = "testgetformfield/submit";
+        $ruleTag = "testruletag";
+        $ruleAction = "testruletag/submit";
+        $ruleScore = 56;
+
+        $rule = RecaptchaV3Rule::create([
+            'Tag' => $ruleTag,
+            'Score' => $ruleScore,
+            'Action' => $ruleAction,
+            'Enabled' => 1,
+            'ActionToTake' => RecaptchaV3Rule::TAKE_ACTION_BLOCK,
+            'AutoCreated' => 0
+        ]);
+        $id = $rule->write();
+
+        $this->assertNotEmpty($id, "Rule was created");
+
+        $field = EditableRecaptchaV3Field::create();
+        $field->Title = "Test spam protection";
+        $field->IncludeInEmails = 0;
+        $field->Score = $fieldScore;
+        $field->Action = $fieldAction;
+        $field->RuleID = $id;
+        $field->write();
+
+        $formField = $field->getFormField();
+
+        $this->assertInstanceOf(RecaptchaV3Field::class, $formField, "Field is a RecaptchaV3Field");
+
+        $score = $formField->getScore();
+        $this->assertEquals(round($ruleScore / 100, 2), $score, "Score matches");
+
+        $action = $formField->getRecaptchaAction();
+        $this->assertEquals($ruleAction, $action, "Action matches");
+
+        $foundRule = $formField->getRecaptchaV3Rule();
+        $this->assertEquals($rule->ID, $foundRule->ID, "Rule matches");
+    }
 
     /**
      * Test field value inclusion/exclusion
